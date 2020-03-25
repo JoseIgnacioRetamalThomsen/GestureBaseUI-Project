69372686eb1aec88ba59603e4f9208bee5a564e2
Pepe <joseretamal@outlook.ie>
2020-03-25 17:23:48 +0000

    wrappers refactored

--- GestureBaseUI-Project/MouseControl/MouseController.cs
+++ GestureBaseUI-Project/MouseControl/MouseController.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
-using System.Runtime.InteropServices;
 using System.Text;
+using static GestureBaseUI_Project.Win32Wrapper.MouseW32W;
+using static GestureBaseUI_Project.Win32Wrapper.WindowW32W;
 
 namespace GestureBaseUI_Project.MouseControl
 {
@@ -65,191 +66,13 @@ namespace GestureBaseUI_Project.MouseControl
         ///  https://stackoverflow.com/questions/8739523/directing-mouse-events-dllimportuser32-dll-click-double-click
         ///
 
-        ///
-        /// Mouse system flags
-        /// 
-        [Flags]
-        public enum MouseEventFlags
-        {
-            LEFTDOWN = 0x00000002,
-            LEFTUP = 0x00000004,
-            MIDDLEDOWN = 0x00000020,
-            MIDDLEUP = 0x00000040,
-            MOVE = 0x00000001,
-            ABSOLUTE = 0x00008000,
-            RIGHTDOWN = 0x00000008,
-            RIGHTUP = 0x00000010,
-            WHEEL = 0x00000800,
-            XDOWN = 0x00000080,
-            HWHEEL = 0x00001000,
-        }
-
-        /// <summary>
-        /// Set position of mouse in screen.
-        /// 
-        ///         /// </summary>
-        /// <param name="x"> mouse position</param>
-        /// <param name="y">mouse positioon</param>
-        /// <returns></returns>
-        [DllImport("user32")]
-        public static extern int SetCursorPos(int x, int y);
-
-        /// <summary>
-        /// Produce mouse events using flags
-        /// 
-        /// http://pinvoke.net/default.aspx/user32/mouse_event.html?diff=y
-        /// </summary>
-        /// <param name="dwFlags"></param>
-        /// <param name="dx"></param>
-        /// <param name="dy"></param>
-        /// <param name="dwData"></param>
-        /// <param name="dwExtraInfo"></param>
-        [DllImport("user32.dll")]
-        static extern void mouse_event(uint dwFlags, uint dx, uint dy, int dwData, int dwExtraInfo);
-
-        //https://stackoverflow.com/a/4232281
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-
-        internal static extern bool GetCursorPos(ref Win32Point pt);
-
-
-
-        /// <summary>
-        /// Modified from http://www.pinvoke.net/default.aspx/user32/GetWindow.html
-        /// Retrieves a handle to a window that has the specified relationship (Z-Order or owner) to the specified window.
-        /// </summary>
-        /// <remarks>The EnumChildWindows function is more reliable than calling GetWindow in a loop. An application that
-        /// calls GetWindow to perform this task risks being caught in an infinite loop or referencing a handle to a window
-        /// that has been destroyed.</remarks>
-        /// <param name="hWnd">A handle to a window. The window handle retrieved is relative to this window, based on the
-        /// value of the uCmd parameter.</param>
-        /// <param name="uCmd">The relationship between the specified window and the window whose handle is to be
-        /// retrieved.</param>
-        /// <returns>
-        /// If the function succeeds, the return value is a window handle. If no window exists with the specified relationship
-        /// to the specified window, the return value is NULL. To get extended error information, call GetLastError.
-        /// </returns>
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr GetWindow(IntPtr hWnd, GetWindowType uCmd);
-
-        private enum GetWindowType : uint
-        {
-            /// <summary>
-            /// The retrieved handle identifies the window of the same type that is highest in the Z order.
-            /// <para/>
-            /// If the specified window is a topmost window, the handle identifies a topmost window.
-            /// If the specified window is a top-level window, the handle identifies a top-level window.
-            /// If the specified window is a child window, the handle identifies a sibling window.
-            /// </summary>
-            GW_HWNDFIRST = 0,
-            /// <summary>
-            /// The retrieved handle identifies the window of the same type that is lowest in the Z order.
-            /// <para />
-            /// If the specified window is a topmost window, the handle identifies a topmost window.
-            /// If the specified window is a top-level window, the handle identifies a top-level window.
-            /// If the specified window is a child window, the handle identifies a sibling window.
-            /// </summary>
-            GW_HWNDLAST = 1,
-            /// <summary>
-            /// The retrieved handle identifies the window below the specified window in the Z order.
-            /// <para />
-            /// If the specified window is a topmost window, the handle identifies a topmost window.
-            /// If the specified window is a top-level window, the handle identifies a top-level window.
-            /// If the specified window is a child window, the handle identifies a sibling window.
-            /// </summary>
-            GW_HWNDNEXT = 2,
-            /// <summary>
-            /// The retrieved handle identifies the window above the specified window in the Z order.
-            /// <para />
-            /// If the specified window is a topmost window, the handle identifies a topmost window.
-            /// If the specified window is a top-level window, the handle identifies a top-level window.
-            /// If the specified window is a child window, the handle identifies a sibling window.
-            /// </summary>
-            GW_HWNDPREV = 3,
-            /// <summary>
-            /// The retrieved handle identifies the specified window's owner window, if any.
-            /// </summary>
-            GW_OWNER = 4,
-            /// <summary>
-            /// The retrieved handle identifies the child window at the top of the Z order,
-            /// if the specified window is a parent window; otherwise, the retrieved handle is NULL.
-            /// The function examines only child windows of the specified window. It does not examine descendant windows.
-            /// </summary>
-            GW_CHILD = 5,
-            /// <summary>
-            /// The retrieved handle identifies the enabled popup window owned by the specified window (the
-            /// search uses the first such window found using GW_HWNDNEXT); otherwise, if there are no enabled
-            /// popup windows, the retrieved handle is that of the specified window.
-            /// </summary>
-            GW_ENABLEDPOPUP = 6
-        }
-
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr SetActiveWindow(IntPtr hWnd);
-
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr GetForegroundWindow();
-
-
-        [DllImport("user32.dll")]
-        private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetDesktopWindow();
-
-        public delegate bool WindowEnumCallback(int hwnd, int lparam);
-        //https://stackoverflow.com/a/10819641
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool EnumWindows(WindowEnumCallback lpEnumFunc, int lParam);
-
-        [DllImport("user32.dll")]
-        public static extern void GetWindowText(int h, StringBuilder s, int nMaxCount);
-
-        [DllImport("user32.dll")]
-        public static extern bool IsWindowVisible(int h);
 
         private List<string> Windows = new List<string>();
         public List<IntPtr> WindowsPtrs = new List<IntPtr>();
 
-        //scren show
 
-        private const int SRCCOPY = 0x00CC0020;
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetWindowDC(IntPtr hWnd);
 
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
 
-        [DllImport("gdi32.dll")]
-        private static extern IntPtr CreateCompatibleDC(IntPtr hDC);
-        [DllImport("gdi32.dll")]
-        private static extern bool DeleteDC(IntPtr hDC);
-        [DllImport("gdi32.dll")]
-        private static extern bool DeleteObject(IntPtr hObject);
-        [DllImport("gdi32.dll")]
-        private static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth, int nHeight);
-        [DllImport("gdi32.dll")]
-        private static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
-        [DllImport("gdi32.dll")]
-        private static extern bool BitBlt(IntPtr hObject, int nXDest, int
-nYDest, int nWidth, int nHeight, IntPtr hObjectSource, int nXSrc, int
-nYSrc, int dwRop);
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct RECT
-        {
-            public int left;
-            public int top;
-            public int right;
-            public int bottom;
-        }
         /// <summary>
         /// makes a screenshot of your current desktop and returns a bitmap
         /// </summary>
---
+++ GestureBaseUI-Project/Win32Wrapper/MouseW32W.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using GestureBaseUI_Project.MouseControl;
+
+namespace GestureBaseUI_Project.Win32Wrapper
+{
+    /// <summary>
+    /// Provide acces to win32 api for controlling mouse actions
+    /// </summary>
+    public sealed class MouseW32W
+    {
+        ///
+        /// Mouse system flags
+        /// 
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            XDOWN = 0x00000080,
+            HWHEEL = 0x00001000,
+        }
+
+        /// <summary>
+        /// Produce mouse events using flags
+        /// 
+        /// http://pinvoke.net/default.aspx/user32/mouse_event.html?diff=y
+        /// </summary>
+        /// <param name="dwFlags"></param>
+        /// <param name="dx"></param>
+        /// <param name="dy"></param>
+        /// <param name="dwData"></param>
+        /// <param name="dwExtraInfo"></param>
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, int dwData, int dwExtraInfo);
+
+        /// <summary>
+        /// Set position of mouse in screen.
+        /// 
+        /// </summary>
+        /// <param name="x"> mouse position</param>
+        /// <param name="y">mouse positioon</param>
+        /// <returns></returns>
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        /// <summary>
+        /// Get the actuam mouse position
+        /// https://stackoverflow.com/a/4232281
+        /// </summary>
+        /// <param name="pt">reference to win32point where the position will be set.</param>
+        /// <returns>true if success</returns>
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public  static extern bool GetCursorPos(ref Win32Point pt);
+
+ 
+    }
+}
