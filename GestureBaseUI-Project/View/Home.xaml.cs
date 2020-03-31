00f42f58eefc5a11fdb98c9373c1e3634bbb45b7
Mark Ndipenoch <G00352031@GMIT.IE>
2020-03-31 15:02:09 +0100

    speech  added

--- GestureBaseUI-Project/View/Home.xaml.cs
+++ GestureBaseUI-Project/View/Home.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Speech.Recognition;
 using GestureBaseUI_Project.ViewModel;
+using System.Speech.Recognition;
+using System.Globalization;
 
 namespace GestureBaseUI_Project
 {
@@ -48,6 +50,56 @@ namespace GestureBaseUI_Project
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new Prediction());
+        }
+
+        //Adapted from https://docs.microsoft.com/en-us/dotnet/api/system.speech.recognition.speechrecognitionengine.recognize?view=netframework-4.8
+        public void Button_Voice_Command_On(object sender, RoutedEventArgs e)
+        {
+            string text;
+    
+            // Create an in-process speech recognizer for the en-US locale.
+            using (SpeechRecognitionEngine recognizer =
+            new SpeechRecognitionEngine())
+                {
+                    // Configure input to the speech recognizer.
+                    recognizer.SetInputToDefaultAudioDevice();
+
+                    // Modify the time value. 
+                    recognizer.BabbleTimeout = TimeSpan.FromSeconds(0);
+                    recognizer.InitialSilenceTimeout = TimeSpan.FromSeconds(5);
+                    recognizer.EndSilenceTimeout = TimeSpan.FromSeconds(0.5);
+
+                    // Create and load a dictation grammar.  
+                    recognizer.LoadGrammar(new DictationGrammar());
+
+                    // Start synchronous speech recognition.  
+                    RecognitionResult result = recognizer.Recognize();
+
+                    if (result != null)
+                    {
+                    text = result.Text.ToLower();
+                        MessageBox.Show(text);
+                        if (text.Contains("record") == true)
+                        {
+                        this.NavigationService.Navigate(new Recorder());
+                    }
+                    else if(text.Contains("record") == true)
+                        {
+                         this.NavigationService.Navigate(new Prediction());
+                        }
+                    else if (text.Contains("view") == true)
+                        {
+                        this.NavigationService.Navigate(new PictureRecorder());
+                        }
+                    }
+                    else
+                    {
+                       //If result does not match
+                        MessageBox.Show("No Spoken word");
+                    }
+                }
+
+    
         }
         #endregion
     }
